b79ba191a67d725d6b8bb7faa2d01ca44d07db1a
Reality <63677818+Th3R3ality@users.noreply.github.com>
2024-05-15 16:28:03 +0200

    started creating level and pickups

--- MonsterMegaAdUnity/Assets/Scripts/PlayerCombat.cs
+++ MonsterMegaAdUnity/Assets/Scripts/PlayerCombat.cs
@@ -5,6 +5,32 @@ using UnityEngine;
 public class PlayerCombat : MonoBehaviour
 {
     [SerializeField] BaseWeapon m_currentWeapon;
+
+    public void GiveNewWeapon(GameObject weaponPrefab)
+    {
+        var weaponObj = Instantiate(weaponPrefab, this.transform);
+        if (weaponObj == null )
+        {
+            Debug.LogException(new System.Exception("Couldn't instantiate weapon prefab"));
+            return;
+        }
+
+        var weapon = GetComponent<BaseWeapon>();
+        if (weapon == null)
+        {
+            Debug.LogException(new System.Exception("Couldn't find type of <BaseWeapon> on instantiated prefab"));
+            Destroy(weaponObj);
+            return;
+        }
+
+        if (m_currentWeapon != null)
+        {
+            Destroy(m_currentWeapon);
+        }
+
+        m_currentWeapon = weapon;
+    }
+
     void Update()
     {
         
